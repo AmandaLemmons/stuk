2900e2962ab4e073e903cce6ef0e70dcf7e4d800
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-07-10 16:19:23 -0500

    added reviews to places

--- stuk/app/models/place.rb
+++ stuk/app/models/place.rb
@@ -6,5 +6,7 @@ class Place < ActiveRecord::Base
   validates_presence_of :address
   belongs_to :user
   geocoded_by :address
-  after_validation :geocode 
+  after_validation :geocode
+
+  has_many :reviews, dependent: :destroy 
 end
--- stuk/config/routes.rb
+++ stuk/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   resources :places
+  resources :reviews 
 root 'pages#home'
   devise_for :users, controllers: {registrations: "registrations"}
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
